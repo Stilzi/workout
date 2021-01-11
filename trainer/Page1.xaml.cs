2bf9fd6d009a33e0490b810d3cf9cb13d134a660
Stilzi <aslan12.g@mail.ru>
2021-01-11 14:38:39 +0300

    ablhckm

---
+++ trainer/Page1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace trainer
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        COMPUTEREntities C = new COMPUTEREntities();
+        public Page1()
+        {
+            InitializeComponent();
+
+            datagrid.ItemsSource = C.MANUFACTURER.ToList();
+        }
+        private void w_Click(object sender, RoutedEventArgs e)
+        {
+            C.SaveChanges();
+        }
+
+        private void q_Click(object sender, RoutedEventArgs e)
+        {
+            MANUFACTURER K = (MANUFACTURER)datagrid.SelectedItem;
+            C.MANUFACTURER.Remove(K);
+            C.SaveChanges();
+            datagrid.ItemsSource = C.MANUFACTURER.ToList();
+        }
+
+        private void e_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2());
+        }
+    }
+}
---
+++ trainer/Page2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace trainer
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        COMPUTEREntities S = new COMPUTEREntities();
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void ins_Click(object sender, RoutedEventArgs e)
+        {
+            MANUFACTURER D = new MANUFACTURER()
+            {
+                NAME = name.Text,
+                ADDRESS = name2.Text,
+                SPECIFICATIONSID = Convert.ToInt32(name3.Text),
+                CONSIGMENTID = Convert.ToInt32(name4.Text)
+            };
+            S.MANUFACTURER.Add(D);
+            S.SaveChanges();
+
+        }
+    }
+}
